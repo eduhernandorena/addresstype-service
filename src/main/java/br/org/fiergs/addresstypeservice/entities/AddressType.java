3b3991340c6e3d3d6450a906e27f837e933f9ce3
Eduardo Hernandorena <eduuardobh@gmail.com>
2019-07-10 10:52:57 -0300

    Padronização das entidades

--- src/main/java/br/org/fiergs/addresstypeservice/entities/AddressType.java
+++ src/main/java/br/org/fiergs/addresstypeservice/entities/AddressType.java
@@ -1,9 +1,6 @@
 package br.org.fiergs.addresstypeservice.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -11,6 +8,7 @@ import javax.persistence.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 @Entity
 @Table(name = "CAD_ADDRESSTYPE")
 @SequenceGenerator(name = "seqCad_AddressType", sequenceName = "SEQCAD_ADDRESSTYPE", allocationSize = 1)
